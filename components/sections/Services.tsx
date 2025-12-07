2836589c5e15255df438f9cbfe2fb471c10629b8
Rafe Loveday <rafe.loveday@icloud.com>
2025-12-07 19:02:49 +0000

    Refactor Services component to improve layout and remove unused code
    - Updated the Services component to fetch Lottie animations dynamically, enhancing loading efficiency.
    - Adjusted the layout of the section and service cards for better responsiveness, switching from a row to a column layout on smaller screens.
    - Removed commented-out code to clean up the component and improve readability.

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -2,8 +2,6 @@
 import { AnimatedDottedBorder } from "@/components/ui/animated-dotted-border";
 import { ServiceCard } from "@/components/ui/service-card";
 import { useState, useEffect } from "react";
-// import WebDevelopmentAnimation from "@/public/images/WebDevelopment.json";
-// import NetworkAnimation from "@/public/images/Network.json";
 
 export default function Services() {
     const [webDevAnimation, setWebDevAnimation] = useState<object | null>(null);
@@ -11,7 +9,6 @@ export default function Services() {
 
     
     useEffect(() => {
-        // Dynamically load Lottie animations using fetch
         Promise.all([
             fetch("/images/WebDevelopment.json").then(res => res.json()),
             fetch("/images/Network.json").then(res => res.json())
@@ -23,12 +20,12 @@ export default function Services() {
         });
     }, []);
     return (
-        <section className="w-full bg-background py-16 md:py-24 lg:py-32 flex flex-col items-center justify-start">
+        <section className="w-full bg-background py-16 px-10 md:py-24 lg:py-32 flex flex-col items-center justify-start">
             <div className="lg:w-7xl w-full flex flex-col items-center justify-start gap-6">
                 <AnimatedDottedBorder className="w-full p-10">
                     <h2 className="text-center">What can we offer for you?</h2>
                 </AnimatedDottedBorder>
-                <div className="w-full flex flex-row gap-4 items-stretch">
+                <div className="w-full flex flex-col md:flex-row gap-4 items-stretch">
                     {webDevAnimation && (
                         <ServiceCard
                             title="Web Development" 
